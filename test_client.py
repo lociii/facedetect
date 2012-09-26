0dc3e1f48a257465e68d627a39debe1a26eb09ae
Steffen Zieger <me@saz.sh>
2012-09-26 15:46:26 +0200

    add test client script

---
+++ test_client.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import urllib2
+import urllib
+import sys
+
+facedetect_url = 'http://localhost:4000'
+
+if len(sys.argv) != 2:
+    print "USAGE: %s <image url>"
+    sys.exit()
+
+data = {'url': sys.argv[1]}
+data = urllib.urlencode(data)
+
+response = None
+try:
+    response = urllib2.urlopen('%s?%s' % (facedetect_url, data))
+except urllib2.HTTPError, e:
+    print e.read()
+
+if response:
+    print response.read()
